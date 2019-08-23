f1fa666e7b95c286a8f8b76fd66db389c737260b
ZHANG Kai <kai.zhang@soprasteria.com>
2019-08-23 16:09:13 +0800

    use ExpansionPanel to replace summary table

--- src/components/SummaryCard.jsx
+++ src/components/SummaryCard.jsx
@@ -4,56 +4,61 @@ import PropTypes from 'prop-types';
  * Material-UI
  */
 import {
-	Card,
 	Grid,
 	Table,
-	TableBody,
 	TableCell,
 	TableHead,
-	TableRow
+	TableRow,
+	ExpansionPanel,
+	ExpansionPanelSummary,
+	ExpansionPanelDetails,
+	Typography
 } from '@material-ui/core';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import TableStyle from '../style/TableStyle';
+import DashBoardStyle from '../style/DashBoardStyle';
 
 const SummaryCard = ({ summary }) => {
-	const classes = TableStyle();
+	const tableClasses = TableStyle();
+	const dashBoardClasses = DashBoardStyle();
 
 	return (
 		<Grid item xs={12} sm={6} md={4} lg={3}>
-			<Card>
-				<Table size='small'>
-					<TableHead>
-						<TableRow>
-							<TableCell
-								size='medium'
-								align='right'
-								className={classes.sizeSmall}
-							>
-								汇总
-							</TableCell>
-						</TableRow>
-					</TableHead>
-					<TableBody>
-						{summary.map(({ id, name, value }) => (
-							<TableRow key={id}>
-								<TableCell
-									size='small'
-									align='right'
-									className={classes.sizeSmall}
-								>
-									{name}
-								</TableCell>
-								<TableCell
-									size='small'
-									align='right'
-									className={classes.sizeSmall}
-								>
-									{value}
-								</TableCell>
-							</TableRow>
-						))}
-					</TableBody>
-				</Table>
-			</Card>
+			<ExpansionPanel>
+				<ExpansionPanelSummary
+					expandIcon={<ExpandMoreIcon />}
+					aria-controls='panel1a-content'
+					id='panel1a-header'
+				>
+					<Typography className={dashBoardClasses.expansionPanelSummary}>
+						汇总
+					</Typography>
+				</ExpansionPanelSummary>
+				<ExpansionPanelDetails>
+					<Table size='small'>
+						<TableHead>
+							{summary.map(({ id, name, value }) => (
+								<TableRow key={id}>
+									<TableCell
+										size='small'
+										align='right'
+										className={tableClasses.sizeSmall}
+									>
+										{name}
+									</TableCell>
+									<TableCell
+										size='small'
+										align='right'
+										className={tableClasses.sizeSmall}
+									>
+										{value}
+									</TableCell>
+								</TableRow>
+							))}
+						</TableHead>
+					</Table>
+				</ExpansionPanelDetails>
+			</ExpansionPanel>
 		</Grid>
 	);
 };
